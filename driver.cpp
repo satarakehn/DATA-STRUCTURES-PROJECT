d65889a2c942f92c8abe74d44d03d627e3b6ac9c
satarakehn <satara@kehn.net>
2021-04-26 13:45:27 -0600

    updated driver

--- driver.cpp
+++ driver.cpp
@@ -19,89 +19,70 @@ void displayMenu(){
 }
 
 int main(int argc, char const *argv[]){
-    string userinput;  
+    bool quit = false; 
+    miniGit s;
+    string userinput; 
+    int input;  
     string filename; 
 
-    while(true){
-        displayMenu();
-        getline(cin,userinput);
-        if(userinput == "1")
-        {
-<<<<<<< HEAD
-            
-            cout << "Enter a filename to be added: " << endl;
-            getline(cin, filename);
-            if (!fs::exists(filename))//check if the file exists
-            {
-                //addFile(filename); 
-            }
-            else
-            {
-                cout << "File already exists." << endl;
-            }
+    while(!quit){
 
-=======
-            miniGit s;
-            string filename;
-            cout << "Enter a filename to be added." << endl;
-            getline(cin, filename);
+        displayMenu();
+        getline(cin, userinput);
+
+        switch(input)
+        {
+            case 1: {
+                //miniGit s;
+                string filename;
+                cout << "Enter a filename to be added." << endl;
+                getline(cin, filename);
            
                 s.addFile(filename);
->>>>>>> d65131bee86d2260031bfa71c30bbb8d35a3bfb6
             
-        }
-        else if(userinput == "2")
-        {
-<<<<<<< HEAD
-            
-            cout << "Enter a filename to be deleted: " << endl;
-            getline(cin, filename);
-            if (!fs::exists(filename))
-            {
-                //removeFile(filename);
-=======
-            miniGit s;
-            string filename;
-            cout << "Enter a filename to be deleted." << endl;
-            getline(cin, filename);
-            if (!fs::exists(filename))
-            {
-                s.removeFile(filename);
->>>>>>> d65131bee86d2260031bfa71c30bbb8d35a3bfb6
+                break; 
             }
-            else
-            {
-                cout << "File already exists." << endl;
+            case 2: {
+                //miniGit s;
+                string filename;
+                cout << "Enter a filename to be deleted." << endl;
+                getline(cin, filename);
+                if (!fs::exists(filename))
+                {
+                    s.removeFile(filename);
+                }
+                else
+                {
+                    cout << "File already exists." << endl;
+                }
+                break; 
             }
-            
-        }
-        else if(userinput == "3")
-        {
-            miniGit s;
-            int answer;
-           cout << "Would you like to commit changes now?" << endl;
-           cout << "Choose 1 for yes and 2 for no" << endl;
-            
-            if (answer == 1)
-            {
-               s.commitChanges();
+            case 3: {
+                int answer;
+                cout << "Would you like to commit changes now?" << endl;
+                cout << "Choose 1 for yes and 2 for no" << endl;
+                
+                if (answer == 1)
+                {
+                    s.commitChanges();
+                }
+                break; 
             }
-            
-            
-            
-        }
-        else if(userinput == "4")
-        {
-            
-        }
-        else if(userinput == "5")
-        {
-            cout << "Goodbye!" << endl; 
-            
-        }
-        else{
-            cout << "Command not found"<< endl;  
-            //or break; 
+            case 4: {
+
+                break; 
+            }
+            case 5: {
+                //quit function 
+                cout << "Goodbye!" << endl; 
+                break; 
+            }
+
+            default: 
+                cout << "Invalid input" << endl; 
+                break; 
         }
     }
+
 }
+
