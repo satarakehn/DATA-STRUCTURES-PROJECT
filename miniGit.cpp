d83507420403a9ec7fc3b9730c362a7721594bb9
satarakehn <satara@kehn.net>
2021-04-27 11:42:58 -0600

    .cpp

--- miniGit.cpp
+++ miniGit.cpp
@@ -170,9 +170,25 @@ void miniGit::commitChanges(int commitNum){
     //update the SLL node member fileVersion to the incremeneted name 
     //once all files have been scanned, create a new DLL node of the repo 
     //an exat copy of the SLL from prev node should be copied into the new DLL node 
-    //the commit number of the new DLL node will be the prev node commit number incremented by one 
+    //the commit number of the new DLL node will be the prev node commit number incremented by one
 
 
+
+    
+
+} 
+
+
+void miniGit::checkout(int commitNumber){
+    // -> this is referencing continuing sentence on next line 
+    //if user chooses to checout a version, the should be prompted to enter a commit number 
+    //if commit number is valid, the files in current directory should be overwritten by the ->
+    //corresponding files from the .minigit directory 
+    //good idea to issue a warning to user that they will lose their local changes if they checkout different ->
+    //version before making a commit with their current local changes 
+    //you will need to search through the DLL for a node with a matching commit number 
+    //you must disallow add/remove/commit operations when the current version is different from the most ->
+    // recent commit (aka the last DLL node)
     cout << "Warning: You will lose your local changes if you checkout a different version before making a commit with current local changes." << endl;
     cout << "Your current version is: " << currVersion->commitNumber << endl;
     
@@ -194,25 +210,6 @@ void miniGit::commitChanges(int commitNum){
             temp = temp->next;
         }
     }
-
-
-
-
-    
-
-} 
-
-
-void miniGit::checkout(int commitNumber){
-    // -> this is referencing continuing sentence on next line 
-    //if user chooses to checout a version, the should be prompted to enter a commit number 
-    //if commit number is valid, the files in current directory should be overwritten by the ->
-    //corresponding files from the .minigit directory 
-    //good idea to issue a warning to user that they will lose their local changes if they checkout different ->
-    //version before making a commit with their current local changes 
-    //you will need to search through the DLL for a node with a matching commit number 
-    //you must disallow add/remove/commit operations when the current version is different from the most ->
-    // recent commit (aka the last DLL node)
 
 
 } 
