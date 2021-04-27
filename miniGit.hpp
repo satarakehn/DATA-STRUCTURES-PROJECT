6a7eb7b39ef25af97783750c5608ec2c4bc619ec
satarakehn <satara@kehn.net>
2021-04-27 09:10:48 -0600

    hpp file

--- miniGit.hpp
+++ miniGit.hpp
@@ -27,23 +27,16 @@ struct singlyNode{
 
 class miniGit{
     private:
+    //doublyNode *head; 
 
     public:
     miniGit(); //constructor
     ~miniGit();//destructor
-<<<<<<< HEAD
     void addFile(string); //add file to repo
     void removeFile(string); //remove file from repo 
-    void commitChanges(); 
+    void commitChanges(int); 
     void checkout(int); //checkout any of the previous versions of the repo 
     
-=======
-    void addFile(string filename); //add file to repo
-    void removeFile(string fileName); //remove file from repo 
-    void commitChanges(); 
-    void checkout(int commitNumber); //checkout any of the previous versions of the repo 
->>>>>>> d65131bee86d2260031bfa71c30bbb8d35a3bfb6
-
 }; 
 
 #endif // !MINIGIT_HPP
