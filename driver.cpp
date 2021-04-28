36d93618453e88abb1445d43b053389bfbbad75b
lpipkorn <66457027+lpipkorn@users.noreply.github.com>
2021-04-27 21:25:55 -0600

    Fixing remove case

--- driver.cpp
+++ driver.cpp
@@ -55,13 +55,13 @@ int main(int argc, char const *argv[]){
                 string filename;
                 cout << "Enter a filename to be deleted:" << endl;
                 getline(cin, filename);
-                if (!fs::exists(filename))
+                if (fs::exists(filename))
                 {
                     s.removeFile(filename);
                 }
                 else
                 {
-                    cout << "File already exists." << endl;
+                    cout << "File doesn't exist." << endl;
                 }
                 break; 
             }
